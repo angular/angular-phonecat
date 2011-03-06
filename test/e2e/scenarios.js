f3e39d73724ddb179ed6e51e0275a8cebdf7c124
Igor Minar <iiminar@gmail.com>
2011-03-05 22:17:18 -0800

    step-3
    - added a search box to demonstrate how:
      - the data-binding works on input fields
      - to use [$filter] function
      - [ng:repeat] automatically shrinks and grows the number of phones in the view
    - added an end-to-end test to:
      - show how end-to-end tests are written and used
      - to prove that the search box and the repeater are correctly wired together

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,4 +4,14 @@ describe('my app', function() {
     browser().navigateTo('../../app/index.html');
   });
 
+
+  it('should filter the phone list as user types into the search box', function() {
+    expect(repeater('.phones li').count()).toBe(5);
+
+    input('query').enter('nexus');
+    expect(repeater('.phones li').count()).toBe(1);
+
+    input('query').enter('motorola');
+    expect(repeater('.phones li').count()).toBe(3);
+  });
 });
