86d4e0d8d708f8ad4f686571c446dd7a049a6160
Igor Minar <iiminar@gmail.com>
2011-03-05 22:42:31 -0800

    step-5 XHR and dependency injection
    - Replaced the in-memory dataset with data loaded from the server (in
      the form of static phone.json file to make this tutorial backend
      agnostic)
      - The json file is loaded using the [$http] service
    - Demonstrate the use of [services][service] and [dependency injection][DI]
      - The [$http] is injected into the controller through [dependency injection][DI]

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -12,13 +12,13 @@ describe('PhoneCat App', function() {
 
 
     it('should filter the phone list as user types into the search box', function() {
-      expect(repeater('.phones li').count()).toBe(3);
+      expect(repeater('.phones li').count()).toBe(20);
 
       input('query').enter('nexus');
       expect(repeater('.phones li').count()).toBe(1);
 
       input('query').enter('motorola');
-      expect(repeater('.phones li').count()).toBe(2);
+      expect(repeater('.phones li').count()).toBe(8);
     });
 
 
