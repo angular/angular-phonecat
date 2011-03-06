302710d2bea7fd28c48f0aeac91ac43ec46ff17f
Igor Minar <iiminar@gmail.com>
2011-03-05 21:03:53 -0800

    step-2
    - Converted static page into dynamic one by:
      - create a root controller for the application
      - extracting the data from HTML into a the controller as a mock dataset
      - convert the static document into a template with the use of `ng:` [directive] (iterate over
        mock data using [ng:repeat] and render it into a view)
    - Added unit test, which mostly shows how one goes about writing a unit test, rather then test
      something of value on our mock dataset.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,1 +1,14 @@
 /* App Controllers */
+
+function PhoneListCtrl() {
+  this.phones = [{"name":"Nexus S",
+                  "snippet":"Fast just got faster with Nexus S. A pure Google experience, Nexus S is the first phone to run Gingerbread (Android 2.3), the fastest version of Android yet."},
+                 {"name":"DROID™ 2 Global by Motorola",
+                  "snippet":"The first smartphone with a 1.2 GHz processor and global capabilities."},
+                 {"name":"DROID™ Pro by Motorola",
+                  "snippet":"The next generation of DOES."},
+                 {"name":"T-Mobile myTouch 4G",
+                  "snippet":"The T-Mobile myTouch 4G is a premium smartphone designed to deliver blazing fast 4G speeds so that you can video chat from practically anywhere, with or without Wi-Fi."},
+                 {"name":"Motorola DEFY™ with MOTOBLUR™",
+                  "snippet":"Are you ready for everything life throws your way?"}];
+}
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,1 +1,10 @@
 /* jasmine specs for controllers go here */
+
+describe('PhoneListCtrl', function(){
+
+  it('should create "phones" model with 5 phones', function() {
+    var ctrl = new PhoneListCtrl();
+    expect(ctrl.phones).toBeDefined();
+    expect(ctrl.phones.length).toBe(5);
+  });
+});
