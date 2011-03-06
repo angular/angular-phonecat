c5f83d5e15b766184947984353221b6db6090404
Igor Minar <iiminar@gmail.com>
2011-03-05 20:26:40 -0800

    empty the angular-seed project
    - Initial [angular-seed] project layout without default app and test
      code

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,12 +3,5 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-  beforeEach(module('myApp.services'));
 
-
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
 });
