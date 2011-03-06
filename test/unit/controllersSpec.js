3d9845758f31363701635e9d3973f3cd988c7b24
Igor Minar <iiminar@gmail.com>
2011-03-05 21:03:53 -0800

    step-2 angular template with repeater
    - Converted the static html list into dynamic one by:
      - creating a root controller for the application
      - extracting the data from HTML into a the controller as an in-memory
        dataset
      - converting the static document into a template with the use of
        `[ng:repeat]` [directive] which iterates over the dataset with phones,
        clones the ng:repeat template for each instance and renders it into the
        view
    - Added a simple unit test to show off how to write tests and run them
      with JsTD (see README.md for instructions)

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,4 +1,11 @@
 /* jasmine specs for controllers go here */
-describe('controllers', function() {
+describe('PhoneCat controllers', function() {
 
+  describe('PhoneListCtrl', function(){
+
+    it('should create "phones" model with 3 phones', function() {
+      var ctrl = new PhoneListCtrl();
+      expect(ctrl.phones.length).toBe(3);
+    });
+  });
 });
