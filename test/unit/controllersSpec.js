b4f0ea32f872d50ff49d7496bc6fc0257f0dc906
Igor Minar <iiminar@gmail.com>
2011-03-06 00:40:40 -0800

    step-4 phone ordering
    - Add "age" property to the phone model
    - Add select box to control phone list order
    - Override the default order value in controller
    - Add unit and e2e test for this feature

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,15 +4,18 @@
 describe('PhoneCat controllers', function() {
 
   describe('PhoneListCtrl', function(){
+    var scope, ctrl;
 
     beforeEach(module('phonecatApp'));
 
-    it('should create "phones" model with 3 phones', inject(function($controller) {
-      var scope = {},
-          ctrl = $controller('PhoneListCtrl', {$scope:scope});
-
-      expect(scope.phones.length).toBe(3);
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {$scope:scope});
     }));
 
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
   });
 });
