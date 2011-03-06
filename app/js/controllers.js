4679a5f3be8839e8627e99010abb804c128638b7
Igor Minar <iiminar@gmail.com>
2011-03-05 21:03:53 -0800

    step-2 angular template with repeater
    - Converted the static html list into dynamic one by:
      - creating a root controller for the application
      - extracting the data from HTML into a the controller as an in-memory
        dataset
      - converting the static document into a template with the use of
        `[ng:repeat]` [directive] which iterates over the dataset with phones,
        clones the ng:repeat template for each instance and renders it into the
        view
    - Added a simple unit test to show off how to write tests and run them
      with JsTD (see README.md for instructions)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,2 +1,11 @@
 'use strict';
 /* App Controllers */
+
+function PhoneListCtrl() {
+  this.phones = [{"name": "Nexus S",
+                  "snippet": "Fast just got faster with Nexus S."},
+                 {"name": "Motorola XOOM™ with Wi-Fi",
+                  "snippet": "The Next, Next Generation tablet."},
+                 {"name": "MOTOROLA XOOM™",
+                  "snippet": "The Next, Next Generation tablet."}];
+}
