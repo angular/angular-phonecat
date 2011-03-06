b7b6066d7ac62582e55c452a436027d98c785a2c
Igor Minar <iiminar@gmail.com>
2011-03-06 00:40:40 -0800

    step-4 phone ordering
    - Add "age" property to the phone model
    - Add select box to control phone list order
    - Override the default order value in controller
    - Add unit and e2e test for this feature

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -20,5 +20,20 @@ describe('PhoneCat App', function() {
       input('query').enter('motorola');
       expect(repeater('.phones li').count()).toBe(2);
     });
+
+
+    it('should be possible to control phone order via the drop down select box', function() {
+      input('query').enter('tablet'); //let's narrow the dataset to make the test assertions shorter
+
+      expect(repeater('.phones li', 'Phone List').column('phone.name')).
+          toEqual(["Motorola XOOM\u2122 with Wi-Fi",
+                   "MOTOROLA XOOM\u2122"]);
+
+      select('orderProp').option('Alphabetical');
+
+      expect(repeater('.phones li', 'Phone List').column('phone.name')).
+          toEqual(["MOTOROLA XOOM\u2122",
+                   "Motorola XOOM\u2122 with Wi-Fi"]);
+    });
   });
 });
