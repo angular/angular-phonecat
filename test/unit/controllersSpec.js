b8bc379c0baf8044d90310e1f77946a662fdcdaa
Igor Minar <iiminar@gmail.com>
2011-03-05 20:26:40 -0800

    step-0 empty angular-seed project
    - Initial [angular-seed] project layout without default app and test
      code

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,29 +1,4 @@
 /* jasmine specs for controllers go here */
+describe('controllers', function() {
 
-describe('MyCtrl1', function(){
-  var myCtrl1;
-
-  beforeEach(function(){
-    myCtrl1 = new MyCtrl1();
-  });
-
-
-  it('should ....', function() {
-    //spec body
-  });
-});
-
-
-describe('MyCtrl2', function(){
-  var myCtrl2;
-
-
-  beforeEach(function(){
-    myCtrl2 = new MyCtrl2();
-  });
-
-
-  it('should ....', function() {
-    //spec body
-  });
 });
