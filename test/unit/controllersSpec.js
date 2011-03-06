989433bf8370ae4a51003e4d8f9a6a900320d504
Igor Minar <iiminar@gmail.com>
2011-03-05 22:42:31 -0800

    step-5 XHR and dependency injection
    - Replaced the in-memory dataset with data loaded from the server (in
      the form of static phone.json file to make this tutorial backend
      agnostic)
      - The json file is loaded using the [$http] service
    - Demonstrate the use of [services][service] and [dependency injection][DI]
      - The [$http] is injected into the controller through [dependency injection][DI]

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,19 +4,26 @@
 describe('PhoneCat controllers', function() {
 
   describe('PhoneListCtrl', function(){
-    var scope, ctrl;
+    var scope, ctrl, $httpBackend;
 
     beforeEach(module('phonecatApp'));
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json').
+          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-    beforeEach(inject(function($controller) {
-      scope = {};
-      ctrl = $controller('PhoneListCtrl', {$scope:scope});
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneListCtrl', {$scope: scope});
     }));
 
 
-    it('should create "phones" model with 3 phones', inject(function($controller) {
-      expect(scope.phones.length).toBe(3);
-    }));
+    it('should create "phones" model with 2 phones fetched from xhr', function() {
+      expect(scope.phones).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.phones).toEqual([{name: 'Nexus S'},
+                                   {name: 'Motorola DROID'}]);
+    });
 
 
     it('should set the default value of orderProp model', function() {
