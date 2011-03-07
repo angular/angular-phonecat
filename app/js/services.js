b3ea9d96e57a32a2a4de9ae4ea1f259ab6407cd9
Igor Minar <iiminar@gmail.com>
2011-03-06 18:15:07 -0800

    step-11 custom service and $resource
    - Replaced [$xhr] with [$resource]
    - Created a custom Phone service that represents the $resource client

--- app/js/services.js
+++ app/js/services.js
@@ -2,3 +2,11 @@
 
 /* Services */
 
+var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+
+phonecatServices.factory('Phone', ['$resource',
+  function($resource){
+    return $resource('phones/:phoneId.json', {}, {
+      query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+    });
+  }]);
