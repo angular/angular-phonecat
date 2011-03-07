78aac83a26fb16ad0fef442a1ddf4fe22319615d
Misko Hevery <misko@hevery.com>
2011-03-06 16:26:30 -0800

    step-7
    - Introduce the [$route] service which allows binding URLs for deep-linking with views
      - Replace content of root controller PhonesCtrl with [$route] configuration
      - Map `/phones' to PhoneListCtrl and partails/phones-list.html
      - Map `/phones/phone-id' to PhoneDetailCtrl and partails/phones-detail.html
      - Copy deep linking parameters to root controller `params` property for access in sub controllers
      - Replace content of index.html with [ng:view]
    - Create PhoneListCtrl view
      - Move code which fetches phones data into PhoneListCtrl
      - Move existing HTML from index.html to partials/phone-list.html
    - Create PhoneDetailsCtrl view
      - Wire [$route] service to map `/phanes/phone-id` to map to this controller.
      - Empty PhoneDetailsCtrl
      - Place holder partials/phane-details.html

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,24 @@
 /* App Controllers */
 
+function PhonesCtrl($route) {
+  var self = this;
+
+  $route.when('/phones',
+             {template:'partials/phone-list.html',   controller:PhoneListCtrl});
+  $route.when('/phones/:phoneId',
+             {template:'partials/phone-detail.html', controller:PhoneDetailCtrl});
+  $route.otherwise({redirectTo:'/phones'});
+
+  $route.onChange(function(){
+    self.params = $route.current.params;
+  });
+
+  $route.parent(this);
+}
+
+//PhonesCtrl.$inject = ['$route'];
+
+
 function PhoneListCtrl($xhr) {
   var self = this;
 
@@ -11,3 +30,8 @@ function PhoneListCtrl($xhr) {
 }
 
 //PhoneListCtrl.$inject = ['$xhr'];
+
+
+function PhoneDetailCtrl() {
+
+}
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,29 +1,69 @@
 /* jasmine specs for controllers go here */
 
-describe('PhoneListCtrl', function(){
+describe('controllers', function(){
   var scope, $browser, ctrl;
 
-  beforeEach(function() {
+  beforeEach(function(){
     scope = angular.scope();
     $browser = scope.$service('$browser');
-
-    $browser.xhr.expectGET('phones/phones.json').respond([{name: 'Nexus S'},
-                                                          {name: 'Motorola DROID'}]);
-    ctrl = scope.$new(PhoneListCtrl);
-
-    expect(ctrl.phones).toBeUndefined();
-    $browser.xhr.flush();
   });
 
 
-  it('should create phones model with 2 phones fetched from xhr', function() {
-    expect(ctrl.phones).toBeDefined();
-    expect(ctrl.phones).toEqual([{name: 'Nexus S'},
-                                 {name: 'Motorola DROID'}]);
+  describe('PhonesCtrl', function(){
+
+    it('should redirect to /phones if no hash', function(){
+      $browser.setUrl('http://myserver.com/');
+      ctrl = scope.$new(PhonesCtrl);
+      $browser.xhr.expectGET('phones/phones.json').respond([]);
+      ctrl.$root.$eval();
+      expect($browser.getUrl()).toEqual('http://myserver.com/#/phones');
+    });
+
+
+    it('should respond to /phones', function(){
+      $browser.setUrl('http://myserver.com/#/phones');
+      ctrl = scope.$new(PhonesCtrl);
+      $browser.xhr.expectGET('phones/phones.json').respond([]);
+      ctrl.$root.$eval();
+      expect(scope.$service('$route').current.controller).toEqual(PhoneListCtrl);
+    });
+
+    it('should respond to /phones/abc', function(){
+      $browser.setUrl('http://myserver.com/#/phones/abc');
+      ctrl = scope.$new(PhonesCtrl);
+      ctrl.$root.$eval();
+      expect(scope.$service('$route').current.controller).toEqual(PhoneDetailCtrl);
+      expect(ctrl.params.phoneId).toEqual('abc');
+    });
   });
 
 
-  it('should set the default value of orderProp model', function() {
-    expect(ctrl.orderProp).toBe('age');
+  describe('PhoneListCtrl', function(){
+    beforeEach(function() {
+      $browser.xhr.expectGET('phones/phones.json').respond([{name: 'Nexus S'},
+                                                            {name: 'Motorola DROID'}]);
+      ctrl = scope.$new(PhoneListCtrl);
+
+      expect(ctrl.phones).toBeUndefined();
+      $browser.xhr.flush();
+    });
+
+
+    it('should create phones model with 2 phones fetched from xhr', function() {
+      expect(ctrl.phones).toBeDefined();
+      expect(ctrl.phones).toEqual([{name: 'Nexus S'},
+                                   {name: 'Motorola DROID'}]);
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(ctrl.orderProp).toBe('age');
+    });
   });
+
+
+  describe('PhoneDetailCtrl', function(){
+
+  });
+
 });
