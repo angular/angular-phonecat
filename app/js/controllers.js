b18678c981ea3d221380f19ac90efeb183dbb59a
Igor Minar <iiminar@gmail.com>
2011-03-06 16:26:30 -0800

    step-7 $route and app partitioning
    - Introduce the [$route] service which allows binding URLs for deep-linking with
      views
      - Create PhoneCatCtrl which governs the entire app and contains $route
        configuration
      - Map `/phones' to PhoneListCtrl and partails/phones-list.html
      - Map `/phones/<phone-id>' to PhoneDetailCtrl and partails/phones-detail.html
      - Copy deep linking parameters to root controller `params` property for access
        in sub controllers
      - Replace content of index.html with [ng:view] widget
    - Create phone list route
      - Preserve existing PhoneListCtrl controller
      - Move existing html from index.html to partials/phone-list.html
    - Create phone details route
      - Empty placeholder PhoneDetailsCtrl controller
      - Empty placeholder partials/phane-details.html template

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,24 @@
 /* App Controllers */
 
+function PhoneCatCtrl($route) {
+  var self = this;
+
+  $route.when('/phones',
+              {template: 'partials/phone-list.html',   controller: PhoneListCtrl});
+  $route.when('/phones/:phoneId',
+              {template: 'partials/phone-detail.html', controller: PhoneDetailCtrl});
+  $route.otherwise({redirectTo: '/phones'});
+
+  $route.onChange(function(){
+    self.params = $route.current.params;
+  });
+
+  $route.parent(this);
+}
+
+//PhoneCatCtrl.$inject = ['$route'];
+
+
 function PhoneListCtrl($xhr) {
   var self = this;
 
@@ -11,3 +30,6 @@ function PhoneListCtrl($xhr) {
 }
 
 //PhoneListCtrl.$inject = ['$xhr'];
+
+
+function PhoneDetailCtrl() {}
