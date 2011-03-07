c67d4727b26e9f551d8f9f994da7d78284a6ce5b
Igor Minar <iiminar@gmail.com>
2011-03-06 18:15:07 -0800

    step-11 custom service and $resource
    - Replaced [$xhr] with [$resource]
    - Created a custom Phone service that represents the $resource client

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,7 +3,16 @@
 /* jasmine specs for controllers go here */
 describe('PhoneCat controllers', function() {
 
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
+
   beforeEach(module('phonecatApp'));
+  beforeEach(module('phonecatServices'));
 
   describe('PhoneListCtrl', function(){
     var scope, ctrl, $httpBackend;
@@ -19,11 +28,11 @@ describe('PhoneCat controllers', function() {
 
 
     it('should create "phones" model with 2 phones fetched from xhr', function() {
-      expect(scope.phones).toBeUndefined();
+      expect(scope.phones).toEqualData([]);
       $httpBackend.flush();
 
-      expect(scope.phones).toEqual([{name: 'Nexus S'},
-                                   {name: 'Motorola DROID'}]);
+      expect(scope.phones).toEqualData(
+          [{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
     });
 
 
@@ -54,10 +63,10 @@ describe('PhoneCat controllers', function() {
 
 
     it('should fetch phone detail', function() {
-      expect(scope.phone).toBeUndefined();
+      expect(scope.phone).toEqualData({});
       $httpBackend.flush();
 
-      expect(scope.phone).toEqual(xyzPhoneData());
+      expect(scope.phone).toEqualData(xyzPhoneData());
     });
   });
 });
