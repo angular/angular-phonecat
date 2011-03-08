b6eddbb8c1a2e617f8d210af98a4c3e2e013290d
Igor Minar <iiminar@gmail.com>
2011-03-08 07:53:42 -0800

    add tutorial related scripts

---
+++ scripts/private/snapshot-web.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ROOT_DIR=`dirname $0`/../..
+cd $ROOT_DIR
+
+SNAP_DIR=angular-phonecat-snapshots-web
+
+
+rm -rf $SNAP_DIR
+mkdir $SNAP_DIR
+
+for i in {0..11}
+do
+  mkdir $SNAP_DIR/step-$i
+  git checkout -f step-$i
+
+    cp -r app test $SNAP_DIR/step-$i/
+    rm -rf $SNAP_DIR/step-$i/test/lib/jstestdriver
+done
