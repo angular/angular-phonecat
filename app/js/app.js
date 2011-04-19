9077fdc1af846ebdf35694b8a5647c1c6191a54e
Igor Minar <iiminar@gmail.com>
2011-04-19 10:54:44 -0700

    step-9 checkmark filter
    - Added custom checkmark filter
    - Update phone detail template to use checkmark filter
    - Added spec for the filter

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,8 @@
 
 var phonecatApp = angular.module('phonecatApp', [
   'ngRoute',
-  'phonecatControllers'
+  'phonecatControllers',
+  'phonecatFilters'
 ]);
 
 phonecatApp.config(['$routeProvider',
