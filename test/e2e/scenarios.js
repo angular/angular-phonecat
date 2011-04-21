e7a52a1545dd4b725a1651b95cea00f6831b959b
Igor Minar <iiminar@gmail.com>
2011-04-21 08:38:17 -0700

    step-10 image swapping with ng:click
    In the phone detail view, clicking on a thumbnail image, changes the
    main phone image to be the large version of the thumbnail image.
    
    - Define mainImageUrl model variable in the PhoneDetailCtrl and set its
      default value
    - Create setImage controller method to change mainImageUrl
    - Register ng:click handler for thumb images to use setImage controller
      method
    - Add e2e tests for this feature
    - Add css to change the mouse cursor when user points at thumnail images

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -61,5 +61,19 @@ describe('PhoneCat App', function() {
     it('should display nexus-s page', function() {
       expect(binding('phone.name')).toBe('Nexus S');
     });
+
+
+    it('should display the first phone image as the main phone image', function() {
+      expect(element('img.phone').attr('src')).toBe('img/phones/nexus-s.0.jpg');
+    });
+
+
+    it('should swap main image if a thumbnail image is clicked on', function() {
+      element('.phone-thumbs li:nth-child(3) img').click();
+      expect(element('img.phone').attr('src')).toBe('img/phones/nexus-s.2.jpg');
+
+      element('.phone-thumbs li:nth-child(1) img').click();
+      expect(element('img.phone').attr('src')).toBe('img/phones/nexus-s.0.jpg');
+    });
   });
 });
