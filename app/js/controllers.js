2987ce71cc61b8a3dff0654a28999455822298d8
Igor Minar <iiminar@gmail.com>
2011-04-21 08:38:17 -0700

    step-10 image swapping with ng:click
    In the phone detail view, clicking on a thumbnail image, changes the
    main phone image to be the large version of the thumbnail image.
    
    - Define mainImageUrl model variable in the PhoneDetailCtrl and set its
      default value
    - Create setImage controller method to change mainImageUrl
    - Register ng:click handler for thumb images to use setImage controller
      method
    - Add e2e tests for this feature
    - Add css to change the mouse cursor when user points at thumnail images

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,7 +16,12 @@ function PhoneListCtrl($scope, $http) {
 function PhoneDetailCtrl($scope, $routeParams, $http) {
   $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
     $scope.phone = data;
+    $scope.mainImageUrl = data.images[0];
   });
+
+  $scope.setImage = function(imageUrl) {
+    $scope.mainImageUrl = imageUrl;
+  }
 }
 
 //PhoneDetailCtrl.$inject = ['$scope', '$routeParams', '$http'];
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -32,11 +32,18 @@ describe('PhoneCat controllers', function() {
 
 
   describe('PhoneDetailCtrl', function(){
-    var scope, $httpBackend, ctrl;
+    var scope, $httpBackend, ctrl,
+        xyzPhoneData = function() {
+          return {
+            name: 'phone xyz',
+                images: ['image/url1.png', 'image/url2.png']
+          }
+        };
+
 
     beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/xyz.json').respond({name:'phone xyz'});
+      $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData());
 
       $routeParams.phoneId = 'xyz';
       scope = $rootScope.$new();
@@ -48,7 +55,7 @@ describe('PhoneCat controllers', function() {
       expect(scope.phone).toBeUndefined();
       $httpBackend.flush();
 
-      expect(scope.phone).toEqual({name:'phone xyz'});
+      expect(scope.phone).toEqual(xyzPhoneData());
     });
   });
 });
