a6a9ef15e200c01f8a8a54523aa96ab780d7b304
TudorCampean <Tudor.Campean@webappsproduction.com>
2013-04-23 14:37:57 +0300

    step-6 phone images and links - adding phone image and links to phone pages - add end2end test that verifies our phone links - css to style the page just a notch

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -35,5 +35,12 @@ describe('PhoneCat App', function() {
           toEqual(["MOTOROLA XOOM\u2122",
                    "Motorola XOOM\u2122 with Wi-Fi"]);
     });
+
+
+    it('should render phone specific links', function() {
+      input('query').enter('nexus');
+      element('.phones li a').click();
+      expect(browser().location().url()).toBe('/phones/nexus-s');
+    });
   });
 });
