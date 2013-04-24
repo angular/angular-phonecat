9edc1bb786a6b91491e989505d06264d9fad7a23
TudorCampean <Tudor.Campean@webappsproduction.com>
2013-04-24 11:40:08 +0300

    step-11 custom service and $resource
    - Replaced [$xhr] with [$resource]
    - Created a custom Phone service that represents the $resource client

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,25 +2,22 @@
 
 /* Controllers */
 
-function PhoneListCtrl($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+function PhoneListCtrl($scope, Phone) {
+  $scope.phones = Phone.query();
 
   $scope.orderProp = 'age';
 }
 
-//PhoneListCtrl.$inject = ['$scope', '$http'];
+//PhoneListCtrl.$inject = ['$scope', 'Phone'];
 
 
-function PhoneDetailCtrl($scope, $routeParams, $http) {
-  $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
-    $scope.phone = data;
-    $scope.mainImageUrl = data.images[0];
+function PhoneDetailCtrl($scope, $routeParams, Phone) {
+  $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+    $scope.mainImageUrl = phone.images[0];
    });
 
   $scope.setImage = function(imageUrl) {
     $scope.mainImageUrl = imageUrl;
   }
 }
-//PhoneDetailCtrl.$inject = ['$scope', '$routeParams'];
+//PhoneDetailCtrl.$inject = ['$scope', '$routeParams','Phone'];
--- app/js/services.js
+++ app/js/services.js
@@ -1,4 +1,9 @@
 'use strict';
 
 /* Services */
-
+angular.module('phonecatServices', ['ngResource']).
+    factory('Phone', function($resource){
+  return $resource('phones/:phoneId.json', {}, {
+    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+  });
+});
