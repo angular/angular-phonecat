60f60c9f6006b42f53410a64206fa479d8332337
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-28 22:57:18 +0000

    chore(bower): use bower for dependencies
    Align the initial repository with the new angular-seed style.

---
+++ test/karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
