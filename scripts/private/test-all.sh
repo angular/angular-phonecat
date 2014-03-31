1ef62a3af59e17cb126f357081b92cec5e2c4aaa
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-31 14:41:20 +0100

    chore(test-all.sh): update to match new tools

--- scripts/private/test-all.sh
+++ scripts/private/test-all.sh
@@ -26,20 +26,20 @@ cd $ROOT_DIR
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 npm install
 
-./scripts/web-server.js > /dev/null &
+node_modules/.bin/http-server -p 8000 &
 WEBSERVER_PID=$!
 
 # Steps 0 and 1 do not have any tests - but going to step 0 copies useful files
 # Step 2 only has unit tests
 git checkout -f step-2
-./node_modules/karma/bin/karma start config/karma.conf.js --single-run
+./node_modules/.bin/karma start test/karma.conf.js --single-run
 
 # Steps 3 through 12 have both unit and e2e tests
 for i in {3..12}
 do
   git checkout -f step-$i
 
-  ./node_modules/karma/bin/karma start config/karma.conf.js --single-run --browsers $BROWSERS --reporters=dots --no-colors --no-color
-  ./node_modules/karma/bin/karma start config/karma-e2e.conf.js --browsers $BROWSERS_E2E --reporters=dots --no-colors --no-color
+  node_modules/.bin/karma start test/karma.conf.js --single-run
+  node_modules/.bin/protractor test/protractor-conf.js
 
 done
