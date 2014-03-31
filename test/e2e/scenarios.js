dca90c34ab7be58b18b28f275c90f982ddd38dac
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-31 14:41:20 +0100

    step-3 interactive search
    - Added a search box to demonstrate how:
      - the data-binding works on input fields
      - to use [filter] filter
      - [ngRepeat] automatically shrinks and grows the number of phones in the view
    - Added an end-to-end test to:
      - show how end-to-end tests are written and used
      - to prove that the search box and the repeater are correctly wired together

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,10 +2,28 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('PhoneCat App', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('app/index.html');
+  describe('Phone list view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
+
+
+    it('should filter the phone list as user types into the search box', function() {
+
+      var phoneList = element.all(by.repeater('phone in phones'));
+      var query = element(by.model('query'));
+
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+    });
   });
-
 });
