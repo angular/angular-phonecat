5472731241b8a900971c12b9b3b21446c996021f
Peter Bacon Darwin <pete@bacondarwin.com>
2014-04-03 20:52:45 +0100

    chore(update-gh-pages): improve helper script
    - Update bower components
    - Run the tests
    - Stop if any sub task fails
    - Push changes to github

--- scripts/private/update-gh-pages.sh
+++ scripts/private/update-gh-pages.sh
@@ -13,15 +13,32 @@
 #
 # You should review these commits and push them to origin/gh-pages if valid.
 
-ROOT_DIR=`dirname $0`/../..
-cd $ROOT_DIR
+# Exit on error and print each command as it is run
+set -ex
 
+ROOT_DIR=`dirname $0`/../..
 SNAP_DIR=angular-phonecat-snapshots-web
 
+# Get into the correct directory
+cd $ROOT_DIR
 
+# Make sure we are on master
+git checkout -f master
+
+# Ensure that all the tool dependencies are there
+npm install
+
+# Update the bower dependencies
+bower update
+
+# Run all the tests on each step
+scripts/private/test-all.sh
+
+# Clean out the snapshot directory if it was left behind
 rm -rf $SNAP_DIR
 mkdir $SNAP_DIR
 
+# Iterate through each step, checking it out and copying it into the temporary snapshot directory
 for i in {0..12}
 do
   mkdir $SNAP_DIR/step-$i
@@ -29,6 +46,8 @@ do
 
   cp -r app $SNAP_DIR/step-$i/
 
+  # In gh-pages the step directories are one directory further down so we must modify the path
+  # to the bower_components in the index.html files.
   node -e "require('shelljs/global'); sed('-i', /\"\\.\\.\\/bower_components/g, '\"../../bower_components', '$SNAP_DIR/step-$i/app/index.html');"
 done
 
@@ -41,10 +60,7 @@ rm -rf $SNAP_DIR
 
 # Commit any changes to the steps
 git add --all step-*
-git commit -m"chore(*): update live steps to new version of tutorial"
-
-# Update the bower dependencies
-bower update
+git commit -m"chore(step): update live steps to new version of tutorial"
 
 # Commit any changes to the bower dependencies
 git add --all bower_components
@@ -54,4 +70,6 @@ git commit -m"chore(bower_components): update bower dependencies"
 # Display an info message, including the last two commits
 echo gh-pages has been updated. See the log below.
 git log -2
-echo Please review the changes and then push to GitHub "git push origin gh-pages"
+
+# Push changes to the gh-pages branch on GitHub
+git push origin gh-pages
