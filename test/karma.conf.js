cf34002e74934130fe704ce39310db51d8e17d38
Peter Bacon Darwin <pete@bacondarwin.com>
2014-04-24 11:05:00 +0100

    step-12 animations

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function(config){
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'app/js/**/*.js',
       'test/unit/**/*.js'
