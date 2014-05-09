933a952b6b4df606f646466fc856148dce17bbeb
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-09 13:15:11 +0100

    chore(test-all): run e2e and unit tests for all steps

--- scripts/private/test-all.sh
+++ scripts/private/test-all.sh
@@ -35,14 +35,8 @@ npm run update-webdriver
 node_modules/.bin/http-server -p 8000 &
 WEBSERVER_PID=$!
 
-# Steps 0 and 1 do not have any tests
-
-# Step 2 only has unit tests
-git checkout -f step-2
-./node_modules/.bin/karma start test/karma.conf.js --single-run --browsers=$BROWSERS
-
-# Steps 3 through 12 have both unit and e2e tests
-for i in {3..12}
+# Run the unit and e2e tests
+for i in {0..12}
 do
   git checkout -f step-$i
 
