a449842f7a119f9067db9ac147d8f6bb90ba359b
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-19 12:53:26 +0100

    step-7 $route and app partitioning
    - Introduce the [$route] service which allows binding URLs for deep-linking with
      views
      - Create PhoneCatCtrl which governs the entire app and contains $route
        configuration
      - Load the ngRoute module
      - Map `/phones' to PhoneListCtrl and partails/phones-list.html
      - Map `/phones/<phone-id>' to PhoneDetailCtrl and partails/phones-detail.html
      - Copy deep linking parameters to root controller `params` property for access
        in sub controllers
      - Replace content of index.html with [ngView] directive
    
    - Create phone list route
      - Preserve existing PhoneListCtrl controller
      - Move existing html from index.html to partials/phone-list.html
    - Create phone details route
      - Empty placeholder PhoneDetailsCtrl controller
      - Empty placeholder partials/phane-details.html template

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,12 +2,18 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
 
-  $scope.orderProp = 'age';
-});
+    $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }]);
