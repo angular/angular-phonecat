82328a06165ec62455a146f9b51cab7b4556ca3b
Igor Minar <iiminar@gmail.com>
2014-05-19 12:53:26 +0100

    step-10 image swapping with ng:click
    In the phone detail view, clicking on a thumbnail image, changes the
    main phone image to be the large version of the thumbnail image.
    
    - Define mainImageUrl model variable in the PhoneDetailCtrl and set its
      default value
    - Create setImage controller method to change mainImageUrl
    - Register ng:click handler for thumb images to use setImage controller
      method
    - Add e2e tests for this feature
    - Add css to change the mouse cursor when user points at thumnail images

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -34,11 +34,18 @@ describe('PhoneCat controllers', function() {
 
 
   describe('PhoneDetailCtrl', function(){
-    var scope, $httpBackend, ctrl;
+    var scope, $httpBackend, ctrl,
+        xyzPhoneData = function() {
+          return {
+            name: 'phone xyz',
+                images: ['image/url1.png', 'image/url2.png']
+          }
+        };
+
 
     beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/xyz.json').respond({name:'phone xyz'});
+      $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData());
 
       $routeParams.phoneId = 'xyz';
       scope = $rootScope.$new();
@@ -50,7 +57,7 @@ describe('PhoneCat controllers', function() {
       expect(scope.phone).toBeUndefined();
       $httpBackend.flush();
 
-      expect(scope.phone).toEqual({name:'phone xyz'});
+      expect(scope.phone).toEqual(xyzPhoneData());
     });
   });
 });
