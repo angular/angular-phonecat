248725da4848d760265d801c3c28ea87bc477d2b
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-19 12:53:26 +0100

    step-8 phone details view
    - Fetch data for and render phone detail view
      - PhoneDetailCtrl controller to fetch details json with [$http] for a specific
        phone
      - template for the phone detailed view
    - CSS to make the phone details page look "pretty"

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -80,8 +80,8 @@ describe('PhoneCat App', function() {
     });
 
 
-    it('should display placeholder page with phoneId', function() {
-      expect(element(by.binding('phoneId')).getText()).toBe('nexus-s');
+    it('should display nexus-s page', function() {
+      expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
     });
   });
 });
