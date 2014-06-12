c46e046397c69124bf4a082b74b42c235388790b
smather <simon.mather@gmail.com>
2014-06-12 12:16:02 +0100

    step-11 custom service and $resource
    - Replaced [$http] with [$resource]
    - Created a custom Phone service that represents the $resource client

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function(config){
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/js/**/*.js',
       'test/unit/**/*.js'
