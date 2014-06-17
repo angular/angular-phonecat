a9503fb40dd28a1075649062c255ea59f3a36442
Peter Bacon Darwin <pete@bacondarwin.com>
2014-06-17 10:18:41 -0700

    step-8 phone details view
    - Fetch data for and render phone detail view
      - PhoneDetailCtrl controller to fetch details json with [$http] for a specific
        phone
      - template for the phone detailed view
    - CSS to make the phone details page look "pretty"

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,10 +3,11 @@
 /* jasmine specs for controllers go here */
 describe('PhoneCat controllers', function() {
 
+  beforeEach(module('phonecatApp'));
+
   describe('PhoneListCtrl', function(){
     var scope, ctrl, $httpBackend;
 
-    beforeEach(module('phonecatApp'));
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
       $httpBackend = _$httpBackend_;
       $httpBackend.expectGET('phones/phones.json').
@@ -33,5 +34,23 @@ describe('PhoneCat controllers', function() {
 
 
   describe('PhoneDetailCtrl', function(){
+    var scope, $httpBackend, ctrl;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/xyz.json').respond({name:'phone xyz'});
+
+      $routeParams.phoneId = 'xyz';
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
+    }));
+
+
+    it('should fetch phone detail', function() {
+      expect(scope.phone).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.phone).toEqual({name:'phone xyz'});
+    });
   });
 });
