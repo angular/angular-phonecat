efb19920ecb53c4a51b5a204c32172f28bf37d82
jenkins <ci-bot@angularjs.org>
2014-09-16 16:08:27 -0700

    chore(step): update live steps to new version of tutorial

--- step-2/app/bower_components/angular-route/angular-route.js
+++ step-2/app/bower_components/angular-route/angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.24
+ * @license AngularJS v1.2.25
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -771,7 +771,6 @@ ngRouteModule.directive('ngView', ngViewFillContentFactory);
                   controllerAs: 'chapter'
                 });
 
-              // configure html5 to get links working on jsfiddle
               $locationProvider.html5Mode(true);
           }])
           .controller('MainCtrl', ['$route', '$routeParams', '$location',
