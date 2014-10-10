d408cd287bcf103828128df9c5b90ec1d1077e53
karrde00 <karrde00@gmail.com>
2014-10-09 21:56:10 -0500

    details pages

--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,22 @@
 
 var phonecatApp = angular.module('phonecatApp', [
 	'ngRoute',
-	'phonecatControllers'
+	'phonecatControllers',
+	'phonecatFilters'
 ]);
+
+phonecatApp.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider.
+			when('/phones', {
+				templateUrl: 'partials/phone-list.html',
+				controller: 'PhoneListCtrl'
+			}).
+			when('/phones/:phoneId', {
+				templateUrl: 'partials/phone-detail.html',
+				controller: 'PhoneDetailCtrl'
+			}).
+			otherwise({
+				redirectTo: '/phones'
+			});
+}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,15 +2,20 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-    function($scope, $http) {
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
 
-        $http.get('phones/phones.json').success(function(data) {
-            $scope.phones = data;
-        });
+    $scope.orderProp = 'age';
+  }]);
 
-        $scope.orderProp = 'age';
-    }
-]);
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+      $scope.phone = data;
+    });
+  }]);
