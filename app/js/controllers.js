1bb92e4e96cb8c50069ededfcd2b72b908b0f9f9
karrde00 <karrde00@gmail.com>
2014-10-09 19:46:24 -0500

    first commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,13 +4,10 @@
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
+  $http.get('phones/phones.json').success(function(data){
+  	$scope.phones = data;
+  });
+  
+  $scope.orderProp ='age';
 });
