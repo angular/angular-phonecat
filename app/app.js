a78ba3002e51f78297a1f275f8b7ad5ed27664c6
Peter Bacon Darwin <pete@bacondarwin.com>
2014-10-18 14:45:31 +0200

    step-2 Angular Templates
    - Convert the static phone list to dynamic by:
      - Creating a `PhoneListController` controller.
      - Extracting the data from HTML into the controller as an in-memory dataset.
      - Converting the static document into a template with the use of the `ngRepeat` directive.
    - Add a simple unit test for the `PhoneListController` controller to show how to write tests and
      run them using Karma (see README.md for instructions).

---
+++ app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Define the `phonecatApp` module
+var phonecatApp = angular.module('phonecatApp', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
