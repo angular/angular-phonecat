f8000cc9c0d48000e6c0a57a6fa3e5b4b97e2f93
Peter Bacon Darwin <pete@bacondarwin.com>
2014-10-19 09:19:49 +0100

    step-6 Two-way Data Binding
    - Add an `age` property to the phone model.
    - Add a drop-down menu to control the phone list order.
    - Override the default order value in controller.
    - Add unit and end-to-end tests for this feature.
    
    Closes #213

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -7,12 +7,19 @@ describe('phoneList', function() {
 
   // Test the controller
   describe('PhoneListController', function() {
+    var ctrl;
 
-    it('should create a `phones` model with 3 phones', inject(function($componentController) {
-      var ctrl = $componentController('phoneList');
-
-      expect(ctrl.phones.length).toBe(3);
+    beforeEach(inject(function($componentController) {
+      ctrl = $componentController('phoneList');
     }));
+
+    it('should create a `phones` model with 3 phones', function() {
+      expect(ctrl.phones.length).toBe(3);
+    });
+
+    it('should set a default value for the `orderProp` model', function() {
+      expect(ctrl.orderProp).toBe('age');
+    });
 
   });
 
