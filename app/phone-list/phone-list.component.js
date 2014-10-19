7e60b73abeb31e56befeb711fe558f22fd31f950
Peter Bacon Darwin <pete@bacondarwin.com>
2014-10-19 09:19:49 +0100

    step-6 Two-way Data Binding
    - Add an `age` property to the phone model.
    - Add a drop-down menu to control the phone list order.
    - Override the default order value in controller.
    - Add unit and end-to-end tests for this feature.
    
    Closes #213

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -9,14 +9,19 @@ angular.
       this.phones = [
         {
           name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
+          snippet: 'Fast just got faster with Nexus S.',
+          age: 1
         }, {
           name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 2
         }, {
           name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
+          snippet: 'The Next, Next Generation tablet.',
+          age: 3
         }
       ];
+
+      this.orderProp = 'age';
     }
   });
