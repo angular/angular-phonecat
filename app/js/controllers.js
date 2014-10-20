1c855e6259fb6db1676bde3d03058e700c9eedcc
guatebus <guatebus@gmail.com>
2014-10-20 20:56:25 +0200

    Added inline annotation to controller definition
    Added inline annotation so the code matches the tutorial at https://docs.angularjs.org/tutorial/step_05

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,10 +4,11 @@
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', 
+  function($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
 
-  $scope.orderProp = 'age';
-});
+    $scope.orderProp = 'age';
+  }]);
