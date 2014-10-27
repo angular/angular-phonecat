41a90a879620cda88a1f510f0a71ff6287b2d862
Benjamin Bauer <benjaminf.bauer@gmail.com>
2014-10-27 23:43:02 +0100

    chore(package): add support for running karma in firefox
    Closes #209

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['Chrome', 'Firefox'],
 
     plugins : [
             'karma-chrome-launcher',
