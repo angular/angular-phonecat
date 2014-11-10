a60f6bb6b9cc4f2a4a477f48780edd3c3a5dd0e7
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-10 09:27:44 +0000

    step-7 $route and app partitioning
    - Introduce the [$route] service which allows binding URLs for deep-linking with
      views
      - Create PhoneCatCtrl which governs the entire app and contains $route
        configuration
      - Load the ngRoute module
      - Map `/phones' to PhoneListCtrl and partails/phones-list.html
      - Map `/phones/<phone-id>' to PhoneDetailCtrl and partails/phones-detail.html
      - Copy deep linking parameters to root controller `params` property for access
        in sub controllers
      - Replace content of index.html with [ngView] directive
    
    - Create phone list route
      - Preserve existing PhoneListCtrl controller
      - Move existing html from index.html to partials/phone-list.html
    - Create phone details route
      - Empty placeholder PhoneDetailsCtrl controller
      - Empty placeholder partials/phane-details.html template

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,10 +4,18 @@
 
 describe('PhoneCat App', function() {
 
+  it('should redirect index.html to index.html#/phones', function() {
+    browser.get('app/index.html');
+    browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/phones');
+      });
+  });
+
+
   describe('Phone list view', function() {
 
     beforeEach(function() {
-      browser.get('app/index.html');
+      browser.get('app/index.html#/phones');
     });
 
 
@@ -61,6 +69,19 @@ describe('PhoneCat App', function() {
       browser.getLocationAbsUrl().then(function(url) {
         expect(url.split('#')[1]).toBe('/phones/nexus-s');
       });
+    });
+  });
+
+
+  describe('Phone detail view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html#/phones/nexus-s');
+    });
+
+
+    it('should display placeholder page with phoneId', function() {
+      expect(element(by.binding('phoneId')).getText()).toBe('nexus-s');
     });
   });
 });
