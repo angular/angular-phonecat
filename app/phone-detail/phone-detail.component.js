aa3a14dea87cf8c27514600627a0f3959336eb7a
Igor Minar <iiminar@gmail.com>
2014-11-10 09:27:44 +0000

    step-12 Event Handlers
    - Make the thumbnail images in the phone detail view clickable:
      - Introduce a `mainImageUrl` property on `PhoneDetailController`.
      - Implement the `setImage()` method for changing the main image.
      - Use `ngClick` on the thumbnails to register a handler that changes the main image.
      - Add an end-to-end test for this feature.

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -9,8 +9,13 @@ angular.
       function PhoneDetailController($http, $routeParams) {
         var self = this;
 
+        self.setImage = function setImage(imageUrl) {
+          self.mainImageUrl = imageUrl;
+        };
+
         $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
           self.phone = response.data;
+          self.setImage(self.phone.images[0]);
         });
       }
     ]
