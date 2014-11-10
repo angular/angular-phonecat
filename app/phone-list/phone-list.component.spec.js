ce6436a0c348237d44727007a11f42bfd15d4e85
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-10 09:27:44 +0000

    step-7 XHR & Dependency Injection
    - Replace the in-memory dataset with data loaded from the server (in the form of a static
      'phone.json' file to keep the tutorial backend agnostic):
      - The JSON data is loaded using the `$http` service.
    - Demonstrate the use of `services` and `dependency injection` (DI):
      - `$http` is injected into the controller through DI.
      - Introduce DI annotation methods: `.$inject` and inline array
    
    Closes #207

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -7,17 +7,27 @@ describe('phoneList', function() {
 
   // Test the controller
   describe('PhoneListController', function() {
-    var ctrl;
+    var $httpBackend, ctrl;
 
-    beforeEach(inject(function($componentController) {
+    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+    // This allows us to inject a service and assign it to a variable with the same name
+    // as the service while avoiding a name conflict.
+    beforeEach(inject(function($componentController, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json')
+                  .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
       ctrl = $componentController('phoneList');
     }));
 
-    it('should create a `phones` model with 3 phones', function() {
-      expect(ctrl.phones.length).toBe(3);
+    it('should create a `phones` property with 2 phones fetched with `$http`', function() {
+      expect(ctrl.phones).toBeUndefined();
+
+      $httpBackend.flush();
+      expect(ctrl.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
     });
 
-    it('should set a default value for the `orderProp` model', function() {
+    it('should set a default value for the `orderProp` property', function() {
       expect(ctrl.orderProp).toBe('age');
     });
 
