6f3b58e1c5066673559676117a5d8bf5becbea4c
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-10 09:27:44 +0000

    step-8 Templating Links & Images
    - Add a phone image and links to phone pages.
    - Add an end-to-end test that verifies the phone links.
    - Tweak the CSS to style the page just a notch.

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,12 +5,12 @@ angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController($http) {
+    controller: ['$http', function PhoneListController($http) {
       var self = this;
       self.orderProp = 'age';
 
       $http.get('phones/phones.json').then(function(response) {
         self.phones = response.data;
       });
-    }
+    }]
   });
