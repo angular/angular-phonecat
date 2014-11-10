bfed542117ff610d0484bf3d1bc14c240b99e5db
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-10 09:27:44 +0000

    step-13 REST and Custom Services
    - Replace `$http` with `$resource`.
    - Create a custom `Phone` service that represents the RESTful client.
    - Use a custom Jasmine equality tester in unit tests to ignore irrelevant properties.

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -5,18 +5,16 @@ angular.
   module('phoneDetail').
   component('phoneDetail', {
     templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$http', '$routeParams',
-      function PhoneDetailController($http, $routeParams) {
+    controller: ['$routeParams', 'Phone',
+      function PhoneDetailController($routeParams, Phone) {
         var self = this;
+        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+          self.setImage(phone.images[0]);
+        });
 
         self.setImage = function setImage(imageUrl) {
           self.mainImageUrl = imageUrl;
         };
-
-        $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
-          self.phone = response.data;
-          self.setImage(self.phone.images[0]);
-        });
       }
     ]
   });
