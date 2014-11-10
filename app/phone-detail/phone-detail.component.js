e04464d14c3313301693e598e2cb973fce90edff
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-10 09:27:44 +0000

    step-10 More Templating
    - Implement fetching data for the selected phone and rendering to the view:
      - Use `$http` in `PhoneDetailController` to fetch the phone details from a JSON file.
      - Create the template for the detail view.
    - Add CSS styles to make the phone detail page look "pretty-ish".

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -4,10 +4,14 @@
 angular.
   module('phoneDetail').
   component('phoneDetail', {
-    template: 'TBD: Detail view for <span>{{$ctrl.phoneId}}</span>',
-    controller: ['$routeParams',
-      function PhoneDetailController($routeParams) {
-        this.phoneId = $routeParams.phoneId;
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    controller: ['$http', '$routeParams',
+      function PhoneDetailController($http, $routeParams) {
+        var self = this;
+
+        $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
+          self.phone = response.data;
+        });
       }
     ]
   });
