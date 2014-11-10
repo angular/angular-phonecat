44715080372a9c694ef588f0e95e19cfb0191b63
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-10 09:27:44 +0000

    step-8 Templating Links & Images
    - Add a phone image and links to phone pages.
    - Add an end-to-end test that verifies the phone links.
    - Tweak the CSS to style the page just a notch.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -52,6 +52,14 @@ describe('PhoneCat Application', function() {
       ]);
     });
 
+    it('should render phone specific links', function() {
+      var query = element(by.model('$ctrl.query'));
+      query.sendKeys('nexus');
+
+      element.all(by.css('.phones li a')).first().click();
+      expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
+    });
+
   });
 
 });
