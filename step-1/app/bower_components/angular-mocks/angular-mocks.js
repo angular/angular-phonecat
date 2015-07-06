3bd107b7625c8fd7a1af532181bafff915bac6fa
jenkins <ci-bot@angularjs.org>
2015-07-06 15:11:11 -0700

    chore(step): update live steps to new version of tutorial

--- step-1/app/bower_components/angular-mocks/angular-mocks.js
+++ step-1/app/bower_components/angular-mocks/angular-mocks.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.1
+ * @license AngularJS v1.4.2
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
--- step-8/app/bower_components/angular-route/angular-route.js
+++ step-8/app/bower_components/angular-route/angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.4.1
+ * @license AngularJS v1.4.2
  * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -596,9 +596,8 @@ function $RouteProvider() {
                 if (angular.isFunction(templateUrl)) {
                   templateUrl = templateUrl(nextRoute.params);
                 }
-                templateUrl = $sce.getTrustedResourceUrl(templateUrl);
                 if (angular.isDefined(templateUrl)) {
-                  nextRoute.loadedTemplateUrl = templateUrl;
+                  nextRoute.loadedTemplateUrl = $sce.valueOf(templateUrl);
                   template = $templateRequest(templateUrl);
                 }
               }
