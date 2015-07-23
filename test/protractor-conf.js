40a1ca3bfb5c72b01e832c92bed7bde92e84efc1
Duncan Atkinson <duncan.atkinson.nt@gmail.com>
2015-07-23 10:33:16 +0100

    Fix failing e2e tests due to slow animations

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,6 +1,18 @@
 exports.config = {
   allScriptsTimeout: 11000,
 
+  onPrepare: function () {
+    // Disable animations so e2e tests run more quickly and do not randomly fail due to animations
+    var disableNgAnimate = function () {
+      angular.module('disableNgAnimate', []).run(['$animate', function ($animate) {
+        $animate.enabled(false);
+      }]);
+    };
+
+    browser.addMockModule('disableNgAnimate', disableNgAnimate);
+  },
+
+
   specs: [
     'e2e/*.js'
   ],
