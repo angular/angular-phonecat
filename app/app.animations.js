6b927897b2535c057f3f9805bc79687f39fa8262
Peter Bacon Darwin <pete@bacondarwin.com>
2016-01-20 14:55:34 +0000

    step-14 Animations
    - Add animations to the application:
      - Animate changes to the phone list, adding, removing and reordering phones with `ngRepeat`.
      - Animate view transitions with `ngView`.
      - Animate changes to the main phone image in the phone detail view.
    - Showcase three different kinds of animations:
      - CSS transition animations.
      - CSS keyframe animations.
      - JavaScript-based animations.

---
+++ app/app.animations.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.
+  module('phonecatApp').
+  animation('.phone', function phoneAnimationFactory() {
+    return {
+      addClass: animateIn,
+      removeClass: animateOut
+    };
+
+    function animateIn(element, className, done) {
+      if (className !== 'selected') return;
+
+      element.css({
+        display: 'block',
+        position: 'absolute',
+        top: 500,
+        left: 0
+      }).animate({
+        top: 0
+      }, done);
+
+      return function animateInEnd(wasCanceled) {
+        if (wasCanceled) element.stop();
+      };
+    }
+
+    function animateOut(element, className, done) {
+      if (className !== 'selected') return;
+
+      element.css({
+        position: 'absolute',
+        top: 0,
+        left: 0
+      }).animate({
+        top: -500
+      }, done);
+
+      return function animateOutEnd(wasCanceled) {
+        if (wasCanceled) element.stop();
+      };
+    }
+  });
