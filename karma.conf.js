f1f8c1e70a8cf932a4fb011f58da70277a48d7d8
Peter Bacon Darwin <pete@bacondarwin.com>
2016-01-20 14:54:29 +0000

    step-9 Routing & Multiple Views
    - Introduce the `$route` service, which allows binding URLs to views for routing and deep-linking:
      - Add the `ngRoute` module as a dependency.
      - Configure routes for the application.
      - Use the `ngView` directive in 'index.html'.
    - Create a phone list route (`/phones`):
      - Map `/phones` to the existing `phoneList` component.
    - Create a phone detail route (`/phones/:phoneId`):
      - Map `/phones/:phoneId` to a new `phoneDetail` component.
      - Create a dummy `phoneDetail` component, which displays the selected phone ID.
      - Pass the `phoneId` parameter to the component's controller via `$routeParams`.

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function(config) {
 
     files: [
       'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
       '**/*.module.js',
       '*!(.module|.spec).js',
