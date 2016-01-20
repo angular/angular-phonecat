2f144fc177320ad631874c9c521a3cd281a94fe4
Peter Bacon Darwin <pete@bacondarwin.com>
2016-01-20 14:54:29 +0000

    step-9 Routing & Multiple Views
    - Introduce the `$route` service, which allows binding URLs to views for routing and deep-linking:
      - Add the `ngRoute` module as a dependency.
      - Configure routes for the application.
      - Use the `ngView` directive in 'index.html'.
    - Create a phone list route (`/phones`):
      - Map `/phones` to the existing `phoneList` component.
    - Create a phone detail route (`/phones/:phoneId`):
      - Map `/phones/:phoneId` to a new `phoneDetail` component.
      - Create a dummy `phoneDetail` component, which displays the selected phone ID.
      - Pass the `phoneId` parameter to the component's controller via `$routeParams`.

---
+++ app/app.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.
+  module('phonecatApp').
+  config(['$routeProvider',
+    function config($routeProvider) {
+      $routeProvider.
+        when('/phones', {
+          template: '<phone-list></phone-list>'
+        }).
+        when('/phones/:phoneId', {
+          template: '<phone-detail></phone-detail>'
+        }).
+        otherwise('/phones');
+    }
+  ]);
