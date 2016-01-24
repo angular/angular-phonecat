23f9017186dacdddab43c7b0a7ec4622d81a7ebc
Peter Bacon Darwin <pete@bacondarwin.com>
2016-01-24 15:36:38 +0200

    step-2 angular template with repeater
    - Converted the static html list into dynamic one by:
      - creating phoneList component and PhoneListCtrl controller for the application
      - extracting the data from HTML into a the controller as an in-memory
        dataset
      - converting the static document into a template with the use of
        `[ngRepeat]` [directive] which iterates over the dataset with phones,
        clones the ngRepeat template for each instance and renders it into the
        view
    - Added a simple unit test to show off how to write tests and run them
      with Karma (see README.md for instructions)

--- app/js/components.js
+++ app/js/components.js
@@ -1,3 +1,19 @@
 'use strict';
 
 /* Components */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.component('phoneList', {
+  controller: 'PhoneListCtrl',
+  templateUrl: 'partials/phone-list.html'
+}).controller('PhoneListCtrl', function() {
+  this.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,11 +1,17 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function() {
 
-describe('controllers', function() {
+  describe('PhoneListCtrl', function(){
 
-  it("should do something", function() {
+    beforeEach(module('phonecatApp'));
+
+    it('should create "phones" model with 3 phones', inject(function($controller) {
+      var ctrl = $controller('PhoneListCtrl');
+
+      expect(ctrl.phones.length).toBe(3);
+    }));
 
   });
-
 });
