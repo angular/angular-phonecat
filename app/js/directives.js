3e6d8c42135baa23a6465e92c0540643388eacf1
Felix Zapata <fzberlinches@gmail.com>
2016-03-03 09:07:57 +0100

    feat(filter): check number of items to show message

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,16 +4,20 @@
 
 var phonecatDirectives = angular.module('phonecatDirectives', []);
 
-  phonecatDirectives.directive('resultList', [function(){
-    return {
-      restrict: 'A',
-      link: function($scope, $element, $attrs) {
-        var ariaStatus = document.querySelector('.aria-status');
-        $scope.$watch('filtered.length', function(length){
-          if($scope.numItems !== undefined){
-            ariaStatus.innerHTML = length + ' item(s) found';
-          }
-        });
-      }
+phonecatDirectives.directive('resultList', [function () {
+  var ariaStatus = document.querySelector('.aria-status');
+  return {
+    restrict: 'A',
+    link: function ($scope, $element, $attrs) {
+      $scope.$watch('filtered.length', function (length) {
+        if(length === 1) {
+          ariaStatus.innerHTML = length + ' item found';
+        } else if(length > 1) {
+          ariaStatus.innerHTML = length + ' items found';
+        } else {
+          ariaStatus.innerHTML = 'No items found';
+        }
+      });
     }
-  }]);
+  }
+}]);
