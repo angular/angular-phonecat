80b52aaa25d7ad2b1c558eeac563ed00d476eaad
Georgios Kalpakas <g.kalpakas@hotmail.com>
2016-03-28 22:58:42 +0300

    step-5 Filtering Repeaters
    - Add a search box to demonstrate:
      - How the data-binding works on input fields.
      - How to use the `filter` filter.
      - How `ngRepeat` automatically shrinks and grows the number of phones in the view.
    - Add an end-to-end test to:
      - Show how end-to-end tests are written and used.
      - Prove that the search box and the repeater are correctly wired together.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,10 +3,28 @@
 // AngularJS E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
-describe('My app', function() {
+describe('PhoneCat Application', function() {
 
-  beforeEach(function() {
-    browser.get('index.html');
+  describe('phoneList', function() {
+
+    beforeEach(function() {
+      browser.get('index.html');
+    });
+
+    it('should filter the phone list as a user types into the search box', function() {
+      var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+      var query = element(by.model('$ctrl.query'));
+
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+    });
+
   });
 
 });
