b971a31f2345c7050cc23beb78029a91644f1787
Georgios Kalpakas <g.kalpakas@hotmail.com>
2016-03-28 21:53:57 +0300

    step-4 Directory and File Organization
    - Refactor the layout of files and directories, applying best practices and techniques that will
      make the application easier to maintain and expand in the future:
      - Put each entity in its own file.
      - Organize code by feature area (instead of by function).
      - Split code into modules that other modules can depend on.
      - Use external templates in `.html` files (instead of inline HTML strings).

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,15 +2,9 @@
 
 // Register `phoneList` component, along with its associated controller and template
 angular.
-  module('phonecatApp').
+  module('phoneList').
   component('phoneList', {
-    template:
-        '<ul>' +
-          '<li ng-repeat="phone in $ctrl.phones">' +
-            '<span>{{phone.name}}</span>' +
-            '<p>{{phone.snippet}}</p>' +
-          '</li>' +
-        '</ul>',
+    templateUrl: 'phone-list/phone-list.template.html',
     controller: function PhoneListController() {
       this.phones = [
         {
