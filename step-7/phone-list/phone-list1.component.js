ed95631fb8014e607947ee2b06b168bbe6789d32
konstantina89f <konstadina89fifli@hotmail.gr>
2016-09-01 18:50:04 +0200

    step-7

--- step-7/phone-list/phone-list1.component.js
+++ step-7/phone-list/phone-list1.component.js
@@ -4,13 +4,14 @@
 angular.
   module('phoneList').
   component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
+    templateUrl: 'phone-list/phone-list1.template.html',
     controller: function PhoneListController($http) {
       var self = this;
       self.orderProp = 'age';
 
       $http.get('phones/phones.json').then(function(response) {
-        self.phones = response.data;
-		});
+        //self.phones = response.data;
+		self.phones = response.data.slice(0, 8);
+      });
     }
   });
