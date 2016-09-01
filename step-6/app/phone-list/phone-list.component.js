58a901b997370351715a5c683fd0135fb6111dda
konstantina89f <konstadina89fifli@hotmail.gr>
2016-09-01 14:13:22 +0200

    step-6

---
+++ step-6/app/phone-list/phone-list.component.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('phoneList').
+  component('phoneList', {
+    templateUrl: 'phone-list/phone-list.template.html',
+    controller: function PhoneListController() {
+      this.phones = [
+        {
+          name: 'Nexus S',
+          snippet: 'Fast just got faster with Nexus S.',
+          age: 1
+        }, {
+          name: 'Motorola XOOM™ with Wi-Fi',
+          snippet: 'The Next, Next Generation tablet.',
+          age: 2
+        }, {
+          name: 'MOTOROLA XOOM™',
+          snippet: 'The Next, Next Generation tablet.',
+          age: 3
+        }
+      ];
+
+      this.orderProp = 'age';
+    }
+  });
