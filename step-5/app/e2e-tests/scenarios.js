25f608521461ddd1c49237e45889a00cfa230dbd
konstantina89f <konstadina89fifli@hotmail.gr>
2016-09-01 13:39:17 +0200

    e2e test

---
+++ step-5/app/e2e-tests/scenarios.js
@@ -0,0 +1,25 @@
+describe('PhoneCat Application', function() {
+
+  describe('phoneList', function() {
+
+    beforeEach(function() {
+      browser.get('index.html');
+    });
+
+    it('should filter the phone list as a user types into the search box', function() {
+      var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+      var query = element(by.model('$ctrl.query'));
+
+      expect(phoneList.count()).toBe(3);
+
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(2);
+    });
+
+  });
+
+});
