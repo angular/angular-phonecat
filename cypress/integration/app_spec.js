2c40de079f882bad9280abf358d1c104e8c6fdea
Jennifer Shehane <shehane.jennifer@gmail.com>
2017-07-21 14:21:03 -0400

    update tests to use suggested command chaining

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,24 +1,21 @@
 describe('PhoneCat Application', function() {
   it('should redirect `index.html` to `index.html#!/phones', function() {
-    cy
-      .visit("/index.html")
-      .hash().should("eq", "#!/phones")
+    cy.visit("/index.html")
+    cy.hash().should("eq", "#!/phones")
   })
 
   describe('View: Phone list', function() {
     beforeEach(function() {
-      cy
-        .visit('/index.html#!/phones')
-        .ng("model", "$ctrl.query").as("q")
+      cy.visit('/index.html#!/phones')
+      cy.ng("model", "$ctrl.query").as("q")
     })
 
     it('should filter the phone list as a user types into the search box', function() {
-      cy
-        .get("ul.phones li").should("have.length", 20)
-        .get("@q").type("nexus")
-        .get("ul.phones li").should("have.length", 1)
-        .get("@q").clear().type("motorola")
-        .get("ul.phones li").should("have.length", 8)
+      cy.get("ul.phones li").should("have.length", 20)
+      cy.get("@q").type("nexus")
+      cy.get("ul.phones li").should("have.length", 1)
+      cy.get("@q").clear().type("motorola")
+      cy.get("ul.phones li").should("have.length", 8)
     })
 
     it('should be possible to control phone order via the drop-down menu', function() {
@@ -28,17 +25,16 @@ describe('PhoneCat Application', function() {
         }).get()
       }
 
-      cy
-        .get("@q").type("tablet")
-        .ng("binding", "phone.name")
+      cy.get("@q").type("tablet")
+      cy.ng("binding", "phone.name")
         .should(function($names){
           expect(getNames($names)).to.deep.eq [
             "Motorola XOOM™ with Wi-Fi",
             "MOTOROLA XOOM™"
           ]
         })
-        .ng("model", "$ctrl.orderProp").select("name")
-        .ng("binding", "phone.name")
+      cy.ng("model", "$ctrl.orderProp").select("name")
+      cy.ng("binding", "phone.name")
         .should(function($names){
           expect(getNames($names)).to.deep.eq [
             "MOTOROLA XOOM™",
@@ -48,12 +44,11 @@ describe('PhoneCat Application', function() {
     })
 
     it('should render phone specific links', function() {
-      cy
-        .get("@q").type("nexus")
-        .ng("repeater", "phone in $ctrl.phones")
-          .should("have.length", 1) // block until the animation is complete
-          .find("a").first().click()
-        .hash().should("match", /phones\/nexus-s/)
+      cy.get("@q").type("nexus")
+      cy.ng("repeater", "phone in $ctrl.phones")
+        .should("have.length", 1) // block until the animation is complete
+        .find("a").first().click()
+      cy.hash().should("match", /phones\/nexus-s/)
     })
   })
 
@@ -67,15 +62,16 @@ describe('PhoneCat Application', function() {
     })
 
     it('should display the first phone image as the main phone image', function() {
-      cy.get("img.phone.selected").should("have.attr", "src").and("match", /img\/phones\/nexus-s.0.jpg/)
+      cy.get("img.phone.selected")
+        .should("have.attr", "src")
+        .and("match", /img\/phones\/nexus-s.0.jpg/)
     })
 
     it('should swap the main image when clicking on a thumbnail imag', function() {
-      cy
-        .get(".phone-thumbs li:nth-child(3) img").click()
-        .get("img.phone.selected").should("have.attr", "src").and("match", /img\/phones\/nexus-s.2.jpg/)
-        .get(".phone-thumbs li:nth-child(1) img").click()
-        .get("img.phone.selected").should("have.attr", "src").and("match", /img\/phones\/nexus-s.0.jpg/)
+      cy.get(".phone-thumbs li:nth-child(3) img").click()
+      cy.get("img.phone.selected").should("have.attr", "src").and("match", /img\/phones\/nexus-s.2.jpg/)
+      cy.get(".phone-thumbs li:nth-child(1) img").click()
+      cy.get("img.phone.selected").should("have.attr", "src").and("match", /img\/phones\/nexus-s.0.jpg/)
     })
   })
 })
